a74198d33388acce5fd376db765b5736c40e47aa
Haise <99102611+ssshaise@users.noreply.github.com>
2026-04-22 19:14:31 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
